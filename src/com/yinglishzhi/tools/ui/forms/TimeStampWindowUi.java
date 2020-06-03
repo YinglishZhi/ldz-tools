236a5a1e508ac2733a5ee30470f65dbb6ad09424
yinglishzhi <yinglishzhi@gmail.com>
2020-06-03 20:43:29 +0800

    timestamp to time

--- src/com/yinglishzhi/tools/ui/forms/TimeStampWindowUi.java
+++ src/com/yinglishzhi/tools/ui/forms/TimeStampWindowUi.java
@@ -4,10 +4,25 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.freedesktop.dbus.interfaces.Local;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
 /**
  * @author LDZ
@@ -53,7 +68,22 @@ public class TimeStampWindowUi extends BaseWindowUi implements ActionListener {
     /**
      * 转换按钮 时间戳 --> 时间
      */
-    private JButton transfer;
+    private JButton timestampToTimeTransfer;
+
+    /**
+     * 时间转时间戳文本
+     */
+    private JTextField timestampToTimeText;
+
+    /**
+     * 时间戳转时间单位
+     */
+    private JComboBox timestampToTimeUnit;
+
+    /**
+     * 时间戳转时间结果
+     */
+    private JTextField timestampToTimeResult;
 
     private Timer AUTO_TIMER;
 
@@ -63,9 +93,48 @@ public class TimeStampWindowUi extends BaseWindowUi implements ActionListener {
      */
     private boolean autoTimeSwitch = true;
 
+    static Function<LocalDateTime, Long> TIME_2_TIMESTAMP_MILLI = l -> l.toInstant(ZoneOffset.of("+8")).toEpochMilli();
+    static Function<LocalDateTime, Long> TIME_2_TIMESTAMP_SECOND = l -> l.toInstant(ZoneOffset.of("+8")).toEpochMilli() / 1000;
+    static Function<Long, LocalDateTime> TIMESTAMP_2_TIME_MILLI = t -> LocalDateTime.ofEpochSecond(t / 1000, 0, ZoneOffset.ofHours(8));
+    static Function<Long, LocalDateTime> TIMESTAMP_2_TIME_SECOND = t -> LocalDateTime.ofEpochSecond(t, 0, ZoneOffset.ofHours(8));
+
+    static BiFunction<LocalDateTime, DateTimeFormatter, String> TIME_2_STRING = (l, f) -> f.format(l);
+
     public TimeStampWindowUi(Project project, Disposable disposable, ToolWindow toolWindow) {
         // 自动定时器
         initAutoTimeStamp();
+        // 时间戳 --> 时间
+        timestampToTimeTransfer.addActionListener(e -> {
+            String timestampString = timestampToTimeText.getText();
+            // 10 or 13 bit timestamp
+            LocalDateTime localDateTime = null;
+            try {
+                long timestamp = Long.parseLong(timestampString);
+                int selectedIndex = timestampToTimeUnit.getSelectedIndex();
+                TimeUnitEnum timeUnit = TimeUnitEnum.getByCode(selectedIndex);
+                switch (timeUnit) {
+                    case SECOND:
+                        // 秒
+                        localDateTime = TIMESTAMP_2_TIME_SECOND.apply(timestamp);
+                        break;
+                    case MILLISECOND:
+                        // 毫秒
+                        localDateTime = TIMESTAMP_2_TIME_MILLI.apply(timestamp);
+                        break;
+                    default:
+                        // 啥都不是
+                }
+                String result = Optional.of(localDateTime).map(l -> TIME_2_STRING.apply(l, ISO_LOCAL_DATE_TIME)).orElse("error");
+                timestampToTimeResult.setEditable(false);
+                timestampToTimeResult.setText(result);
+            } catch (NumberFormatException numberFormatException) {
+                // TODO: 2020/6/3 格式不对哦
+                timestampToTimeResult.setText("your mother's input is wrong!!");
+
+            }
+
+
+        });
 
     }
 
@@ -85,6 +154,7 @@ public class TimeStampWindowUi extends BaseWindowUi implements ActionListener {
         });
     }
 
+
     public void switchAuto(boolean autoReadSwitch) {
         if (autoReadSwitch) {
             AUTO_TIMER.start();
@@ -103,5 +173,40 @@ public class TimeStampWindowUi extends BaseWindowUi implements ActionListener {
         return container;
     }
 
+    enum TimeUnitEnum {
 
+        /**
+         *
+         */
+        MILLISECOND(0, "毫秒"),
+
+        /**
+         *
+         */
+        SECOND(1, "秒"),
+        ;
+
+        private Integer code;
+        private String desc;
+
+        TimeUnitEnum(Integer code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        private static Map<Integer, TimeUnitEnum> ENUM_MAP = Arrays.stream(values())
+                .collect(Collectors.toMap(TimeUnitEnum::getCode, Function.identity()));
+
+        public static TimeUnitEnum getByCode(Integer code) {
+            return ENUM_MAP.getOrDefault(code, null);
+        }
+    }
 }
