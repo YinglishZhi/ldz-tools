9102b00388d4d67e85050e3f934882e8c1f64719
yinglishzhi <yinglishzhi@gmail.com>
2020-06-03 19:33:48 +0800

    auto time stamp

--- src/com/yinglishzhi/tools/ui/forms/TimeStampWindowUi.java
+++ src/com/yinglishzhi/tools/ui/forms/TimeStampWindowUi.java
@@ -1,36 +1,107 @@
 package com.yinglishzhi.tools.ui.forms;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author LDZ
  * @date 2020/6/3 2:13 下午
  */
-public class TimeStampWindowUi extends BaseWindowUi {
+public class TimeStampWindowUi extends BaseWindowUi implements ActionListener {
 
-
+    /**
+     * 容器
+     */
     private JPanel container;
+
+    /**
+     * 主面板
+     */
     private JPanel mainPanel;
+
+    /**
+     * 当前 label
+     */
     private JLabel nowLabel;
-    private JLabel label1;
+
+    /**
+     * 控制按钮 用来控制 现在时间自动更新
+     */
     private JButton control;
+
+    /**
+     * 时间戳 label
+     */
     private JLabel timeStamp;
+
+    /**
+     * 时间 label
+     */
     private JLabel Label3;
+
+    /**
+     * 当前时间时间戳 自动更新用
+     */
     private JTextField nowTimeStamp;
-    private JLabel l;
+
+    /**
+     * 转换按钮 时间戳 --> 时间
+     */
     private JButton transfer;
-    private static final Icon HIDE = new ImageIcon(JsonParseWindowUi.class.getResource("/icons/tool.png"));
+
+    private Timer AUTO_TIMER;
+
+
+    /**
+     * 默认自动时间戳启动
+     */
+    private boolean autoTimeSwitch = true;
 
     public TimeStampWindowUi(Project project, Disposable disposable, ToolWindow toolWindow) {
-//        button1.setIcon(HIDE);
+        // 自动定时器
+        initAutoTimeStamp();
+
+    }
+
+
+    private void initAutoTimeStamp() {
+        // 文本不可编辑
+        nowTimeStamp.setEditable(false);
+        // 定时器启动
+        AUTO_TIMER = new Timer(1000, this);
+        AUTO_TIMER.start();
+        // 定时器控制按键
+        control.setIcon(AllIcons.General.InspectionsOK);
+        control.addActionListener(e -> {
+            autoTimeSwitch = !autoTimeSwitch;
+            switchAuto(autoTimeSwitch);
+            control.setIcon(autoTimeSwitch ? AllIcons.General.InspectionsOK : AllIcons.General.InspectionsTrafficOff);
+        });
+    }
+
+    public void switchAuto(boolean autoReadSwitch) {
+        if (autoReadSwitch) {
+            AUTO_TIMER.start();
+        } else {
+            AUTO_TIMER.stop();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        nowTimeStamp.setText(System.currentTimeMillis() + "");
     }
 
     @Override
     public JPanel getContent() {
         return container;
     }
+
+
 }
